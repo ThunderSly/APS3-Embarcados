57c1ee3be6251aa4a1f044b1b1886de01dcd6afc
ThunderSly <brunex887@gmail.com>
2019-05-28 23:07:58 -0300

    Task primitivas criadas e Afec e botao implementado

--- WIFI-RTOS-get-example/WIFI-RTOS-get-example/src/main.c
+++ WIFI-RTOS-get-example/WIFI-RTOS-get-example/src/main.c
@@ -38,7 +38,25 @@ static char server_host_name[] = MAIN_SERVER_NAME;
 
 
 #define TASK_WIFI_STACK_SIZE            (4096/sizeof(portSTACK_TYPE))
+#define TASK_LCD_STACK_SIZE            (2*1024/sizeof(portSTACK_TYPE))
+#define TASK_LCD_STACK_PRIORITY        (tskIDLE_PRIORITY)
 #define TASK_WIFI_STACK_PRIORITY        (tskIDLE_PRIORITY)
+
+//AFEC
+#define VOLT_REF        (3300)
+#define MAX_DIGITAL     (4095)
+#define AFEC_CHANNEL_TEMP_SENSOR 0
+
+//BUT
+#define BUT2_PIO           PIOA                  // periferico que controla o LED
+#define BUT2_PIO_ID        ID_PIOA                    // ID do periférico PIOC (controla LED)
+#define BUT2_PIO_IDX       11u                    // ID do LED no PIO
+#define BUT2_PIO_IDX_MASK  (1u << BUT2_PIO_IDX)  // Mascara para CONTROLARMOS o LED
+
+volatile uint32_t g_ul_value = 0;
+QueueHandle_t xQueueAfec;
+QueueHandle_t xQueueBut;
+SemaphoreHandle_t xSemaphoreBut;
 
 extern void vApplicationStackOverflowHook(xTaskHandle *pxTask,
 signed char *pcTaskName);
@@ -89,6 +107,16 @@ extern void vApplicationMallocFailedHook(void)
 	configASSERT( ( volatile void * ) NULL );
 }
 
+static void AFEC_Temp_callback(void)
+{
+	g_ul_value = afec_channel_get_value(AFEC0, AFEC_CHANNEL_TEMP_SENSOR);
+	xQueueSendFromISR( xQueueAfec, &g_ul_value, 0);
+}
+
+void but_callback2(void){
+	xSemaphoreGiveFromISR(xSemaphoreBut, 0);
+}
+
 
 /**
  * \brief Configure UART console.
@@ -105,6 +133,77 @@ static void configure_console(void)
 	/* Configure UART console. */
 	sysclk_enable_peripheral_clock(CONSOLE_UART_ID);
 	stdio_serial_init(CONF_UART, &uart_serial_options);
+}
+
+static void config_ADC_TEMP_RES(void){
+/*************************************
+   * Ativa e configura AFEC
+   *************************************/
+  /* Ativa AFEC - 0 */
+	afec_enable(AFEC0);
+
+	/* struct de configuracao do AFEC */
+	struct afec_config afec_cfg;
+
+	/* Carrega parametros padrao */
+	afec_get_config_defaults(&afec_cfg);
+
+	/* Configura AFEC */
+	afec_init(AFEC0, &afec_cfg);
+
+	/* Configura trigger por software */
+	afec_set_trigger(AFEC0, AFEC_TRIG_SW);
+
+	/* configura call back */
+	afec_set_callback(AFEC0, AFEC_INTERRUPT_EOC_0,	AFEC_Temp_callback, 5);
+
+	/*** Configuracao específica do canal AFEC ***/
+	struct afec_ch_config afec_ch_cfg;
+	afec_ch_get_config_defaults(&afec_ch_cfg);
+	afec_ch_cfg.gain = AFEC_GAINVALUE_0;
+	afec_ch_set_config(AFEC0, AFEC_CHANNEL_TEMP_SENSOR, &afec_ch_cfg);
+
+	/*
+	* Calibracao:
+	* Because the internal ADC offset is 0x200, it should cancel it and shift
+	 down to 0.
+	 */
+	afec_channel_set_analog_offset(AFEC0, AFEC_CHANNEL_TEMP_SENSOR, 0x200);
+
+	/***  Configura sensor de temperatura ***/
+	struct afec_temp_sensor_config afec_temp_sensor_cfg;
+
+	afec_temp_sensor_get_config_defaults(&afec_temp_sensor_cfg);
+	afec_temp_sensor_set_config(AFEC0, &afec_temp_sensor_cfg);
+
+	/* Selecina canal e inicializa conversão */
+	afec_channel_enable(AFEC0, AFEC_CHANNEL_TEMP_SENSOR);
+}
+
+void io_init(void)
+{
+	// Inicializa clock do periférico PIO responsavel pelo botao
+	pmc_enable_periph_clk(BUT2_PIO_ID);
+
+	// Configura PIO para lidar com o pino do botão como entrada
+	// com pull-up
+	pio_configure(BUT2_PIO, PIO_INPUT, BUT2_PIO_IDX_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+
+	// Configura interrupção no pino referente ao botao e associa
+	// função de callback caso uma interrupção for gerada
+	// a função de callback é a: but_callback()
+	pio_handler_set(BUT2_PIO, BUT2_PIO_ID, BUT2_PIO_IDX_MASK, PIO_IT_FALL_EDGE,	but_callback2);
+	
+	NVIC_EnableIRQ(BUT2_PIO_ID);
+	NVIC_SetPriority(BUT2_PIO_ID, 5); // Prioridade 4
+
+	// Ativa interrupção
+	pio_enable_interrupt(BUT2_PIO, BUT2_PIO_IDX_MASK);
+	
+	pio_get_interrupt_status(BUT2_PIO);
+
+	// Configura NVIC para receber interrupcoes do PIO do botao
+	// com prioridade 4 (quanto mais próximo de 0 maior)
 }
 
 
@@ -331,6 +430,26 @@ static void task_monitor(void *pvParameters)
 	}
 }
 
+static void task_afec(void){
+	config_ADC_TEMP_RES();
+	const TickType_t xDelay = 4000 / portTICK_PERIOD_MS;
+	for(;;){
+		afec_start_software_conversion(AFEC0);
+		vTaskDelay(xDelay);
+	}
+}
+
+void task_button(void){
+
+	xSemaphoreBut = xSemaphoreCreateBinary();
+	io_init();
+
+	for(;;){
+		if(xSemaphoreTake(xSemaphoreBut, ( TickType_t ) 500) == pdTRUE ){
+			//xQueueSendFromISR( xQueueBut, &temp, 0);
+		}
+	}
+}
 
 
 static void task_wifi(void *pvParameters) {
@@ -417,6 +536,14 @@ int main(void)
 	if (xTaskCreate(task_wifi, "Wifi", TASK_WIFI_STACK_SIZE, NULL,
 	TASK_WIFI_STACK_PRIORITY, NULL) != pdPASS) {
 		printf("Failed to create Wifi task\r\n");
+	}
+	
+	if (xTaskCreate(task_afec, "afec", TASK_LCD_STACK_SIZE, NULL, TASK_LCD_STACK_PRIORITY, NULL) != pdPASS) {
+		printf("Failed to create test afec task\r\n");
+	}
+	
+	if (xTaskCreate(task_button, "buttons", TASK_LCD_STACK_SIZE, NULL, TASK_LCD_STACK_PRIORITY, NULL) != pdPASS) {
+		printf("Failed to create test buttons task\r\n");
 	}
 
 	vTaskStartScheduler();
